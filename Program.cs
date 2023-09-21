8e8e0f1940cd4ea890c35c87cbc50b023e082b83
Jackson Henderson <jbhhenderson@gmail.com>
2023-09-21 10:58:37 -0500

    Phase 2 complete

--- Program.cs
+++ Program.cs
@@ -33,9 +33,6 @@ Challenge favoriteBeatle = new Challenge(
 int minAwesomeness = 0;
 int maxAwesomeness = 100;
 
-// Make a new "Adventurer" object using the "Adventurer" class
-Adventurer theAdventurer = new Adventurer("Jack");
-
 // A list of challenges for the Adventurer to complete
 // Note we can use the List class here because have the line "using System.Collections.Generic;" at the top of the file.
 List<Challenge> challenges = new List<Challenge>()
@@ -46,6 +43,13 @@ List<Challenge> challenges = new List<Challenge>()
     guessRandom,
     favoriteBeatle
 };
+
+// prompt user for a name
+Console.WriteLine("What is your adventurer's name?");
+string advName = Console.ReadLine();
+
+// Make a new "Adventurer" object using the "Adventurer" class
+Adventurer theAdventurer = new Adventurer(advName);
 
 // Loop through all the challenges and subject the Adventurer to them
 foreach (Challenge challenge in challenges)
